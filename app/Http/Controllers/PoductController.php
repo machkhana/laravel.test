6d10619e735c461c66cc1cd37706cb8021816c6e
machkhana <g.machkhaneli@gmail.com>
2019-02-05 12:27:05 +0400

    test

--- app/Http/Controllers/PoductController.php
+++ app/Http/Controllers/PoductController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class PoductController extends Controller
 {
-    //
+    public function index(){
+        return view('product.index');
+    }
+    public function show(int $prodid){
+        return view('product.show')->with('prodid',$prodid);
+    }
 }
